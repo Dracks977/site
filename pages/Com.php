4226dd594361567f6133127c84901640fdecc9fb
Nathan VAN HEMELRYCK <vanhem_n@g02-r2p10.local>
2015-12-03 11:01:02 +0100

    com

--- pages/Com.php
+++ pages/Com.php
@@ -3,12 +3,10 @@ include('config.php');
 
 $conn = new PDO("mysql:host=$dbhost;dbname=$dbname;charset=utf8",$dbuser,$dbpass);
 $reponse = $conn->query("SELECT * FROM Commentaire_Produit WHERE ID_produit={$id}");
-$rows = $reponse->fetch(PDO::FETCH_NUM);
-if($rows > 0) {
 
-	while ($donnees = $reponse->fetch()){
+
+while ($donnees = $reponse->fetch()){
 	echo $donnees['Description'];
-	}
 }
 
 $reponse->closeCursor();
