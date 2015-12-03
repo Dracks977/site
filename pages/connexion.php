bc0868fb2acdf97cd92240f6e3d260ce3d58a05e
Nathan VAN HEMELRYCK <vanhem_n@g02-r2p10.local>
2015-12-03 13:17:03 +0100

    graphique correction

--- pages/connexion.php
+++ pages/connexion.php
@@ -36,7 +36,7 @@ include("headerD.php");
 if( isset($_SESSION['ERRMSG_ARR']) && is_array($_SESSION['ERRMSG_ARR']) && count($_SESSION['ERRMSG_ARR']) >0 ) {
   echo '<ul style="padding:0; color:red;">';
   foreach($_SESSION['ERRMSG_ARR'] as $msg) {
-    echo '<li>',$msg,'</li>'; 
+    echo '<li><p>',$msg,'</p></li>'; 
   }
   echo '</ul>';
   unset($_SESSION['ERRMSG_ARR']);
