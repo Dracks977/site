e4d60d386e05c9b534022cfef5c83b00704cdd9c
Nathan VAN HEMELRYCK <vanhem_n@g02-r2p10.local>
2015-12-03 11:40:13 +0100

    com

--- pages/Lcom.php
+++ pages/Lcom.php
@@ -1,45 +1,45 @@
 <?php
 session_start();
-include('config.php');
-$errmsg_arr = array();
-$errflag = false;
-// configuration
-include('config.php');
- try {
-    $conn = new PDO("mysql:host=$dbhost;dbname=$dbname",$dbuser,$dbpass);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully"; 
-    }
-catch(PDOException $e)
-    {
-    echo "Connection failed (check config.php):    " . $e->getMessage();
-    exit();
-    }
+// include('config.php');
+// $errmsg_arr = array();
+// $errflag = false;
+// // configuration
+// include('config.php');
+//  try {
+//     $conn = new PDO("mysql:host=$dbhost;dbname=$dbname",$dbuser,$dbpass);
+//     // set the PDO error mode to exception
+//     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//     echo "Connected successfully"; 
+//     }
+// catch(PDOException $e)
+//     {
+//     echo "Connection failed (check config.php):    " . $e->getMessage();
+//     exit();
+//     }
 var_dump($_SESSION);
-$id = $_GET['id']
-$Nom = $_SESSION['User_Nom'];  
-$Prenom = $_SESSION['User_Prenom'];
-$Description = $_POST['texte']; 
+// $id = $_GET['id']
+// $Nom = $_SESSION['User_Nom'];  
+// $Prenom = $_SESSION['User_Prenom'];
+// $Description = $_POST['texte']; 
 
-if ($Description == '') {
-	$errmsg_arr[] = 'Vous avez oublié votre commentaire';
-	$errflag = true;
-}
-else{
+// if ($Description == '') {
+// 	$errmsg_arr[] = 'Vous avez oublié votre commentaire';
+// 	$errflag = true;
+// }
+// else{
 
-	$result = $conn->prepare("INSERT INTO Commentaire_Produit (ID_produit,Nom,Prenom,Description) VALUES ({$id}, :Nom, :Prenom, :Description);");
-	$result->bindParam(':Description', $Description);
-	$result->bindParam(':Nom', $_SESSION['User_Nom']);
-	$result->bindParam(':Prenom', $Prenom);
-	$result->execute();
-		if($errflag) {
-		$_SESSION['ERRMSG_ARR'] = $errmsg_arr;
-		header("location: dproduit.php?p=" . $id);
-		exit();
-	}
-		else{
-		header("location: dproduit.php?p=" . $id);
-	}
-}
+// 	$result = $conn->prepare("INSERT INTO Commentaire_Produit (ID_produit,Nom,Prenom,Description) VALUES ({$id}, :Nom, :Prenom, :Description);");
+// 	$result->bindParam(':Description', $Description);
+// 	$result->bindParam(':Nom', $_SESSION['User_Nom']);
+// 	$result->bindParam(':Prenom', $Prenom);
+// 	$result->execute();
+// 		if($errflag) {
+// 		$_SESSION['ERRMSG_ARR'] = $errmsg_arr;
+// 		header("location: dproduit.php?p=" . $id);
+// 		exit();
+// 	}
+// 		else{
+// 		header("location: dproduit.php?p=" . $id);
+// 	}
+// }
 ?>
