1e6467eece03139b4186ab0deffc5e0be53049ad
Nathan VAN HEMELRYCK <vanhem_n@g02-r2p10.local>
2015-12-04 09:29:44 +0100

    categ dynamique #1

---
+++ pages/categ.php
@@ -0,0 +1,19 @@
+<?php
+
+$conn = new PDO("mysql:host=$dbhost;dbname=$dbname;charset=utf8",$dbuser,$dbpass);
+$reponse = $conn->query("SELECT * FROM Categories WHERE ID>2");
+while ($donnees = $reponse->fetch()){
+
+}
+$reponse->closeCursor();
+
+  echo "<a href='listprod01.html?id=" . $donnees['ID'] . "'><section id='section03'>
+       <aside>
+         <h1 id='tpop'>" . $donnees['Libelle'] . "</h1>
+         <center><image src='" . $donnees['image'] . "' width='170'></image></center>
+       </aside>
+       <article>                
+         <center><h4>" . $donnees['Description'] . "</h4></center>
+       </article>
+        </section></a>"
+?>
