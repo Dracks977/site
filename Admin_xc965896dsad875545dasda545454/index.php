31906ab6e6816fcca257a85fc20e6de6d087c9b5
Nathan VAN HEMELRYCK <vanhem_n@g02-r2p10.local>
2015-12-04 16:06:07 +0100

    Admin

--- Admin_xc965896dsad875545dasda545454/index.php
+++ Admin_xc965896dsad875545dasda545454/index.php
@@ -15,16 +15,19 @@
 <!-- Header --> 
 <div id='header'>
 
-<a href="#" class="bouton blanc large">Gestion des Users</a>
-<a href="#" class="bouton blanc large">Suppression elements</a>
-<a href="#" class="bouton blanc large">Ajout elements</a>
-<a href="#" class="bouton blanc large">Modification elements</a>
-<a href="#" class="bouton blanc large" style='margin-left : 35%'>Retour au site</a>
+<a href="Admin_xc965896dsad875545dasda545454/index.php?id=1" class="bouton blanc large">Gestion des Users</a>
+<a href="Admin_xc965896dsad875545dasda545454/index.php?id=2" class="bouton blanc large">Suppression elements</a>
+<a href="Admin_xc965896dsad875545dasda545454/index.php?id=3" class="bouton blanc large">Ajout elements</a>
+<a href="Admin_xc965896dsad875545dasda545454/index.php?id=4" class="bouton blanc large">Modification elements</a>
+<a href="Admin_xc965896dsad875545dasda545454/index.php?id=5" class="bouton blanc large" style='margin-left : 35%'>Retour au site</a>
 </div>
 
 <center><div id='wrap'>
 <!-- div categorie-->
  <section id='moite'></section>
+ <?php if (!isset($_GET['id']) OR $_GET['id'] == 1): ?>
+ 	lol
+<?php endif; ?>
  <!-- div produit-->
  <section id='moite'></section>
 
