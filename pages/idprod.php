df72df306849b1f947452ab0671630eb2b8c5aeb
Nathan VAN HEMELRYCK <vanhem_n@g02-r2p10.local>
2015-12-04 11:01:19 +0100

    pannier

--- pages/idprod.php
+++ pages/idprod.php
@@ -6,6 +6,7 @@ $reponse = $conn->query("SELECT * FROM Produits WHERE ID={$id}");
 $donnees = $reponse->fetch();
 $reponse->closeCursor();
 
+if (isset($_SESSION['c'])){
 echo "<section id='top'>
 		<aside>
 			<h1 id='toph'>" . $donnees['Libelle'] . "</h1>
@@ -25,4 +26,26 @@ echo "<section id='top'>
             	</h4>
           	</article>
 		</section>";
+  }
+  else{
+echo "<section id='top'>
+    <aside>
+      <h1 id='toph'>" . $donnees['Libelle'] . "</h1>
+    </aside>
+    </section>
+    <section id='con'>
+            <aside>
+              <image id='marg2' src='". $donnees['image'] ."' width='20%''></image>
+            </aside>
+            <article>
+              <h4 id='dprod'>" . $donnees['Libelle'] . "<br />" . $donnees['Description'] . 
+                "<ul>
+                    <form id='font1' action='connexion.php' method='POST'> 
+                    <li><input type='submit' value='ajouter au panier'></li>
+                    <li id='prix'>Prix : " . $donnees['Prix'] . "€</li>
+                  </ul>
+              </h4>
+            </article>
+    </section>";
+  }
 ?>
