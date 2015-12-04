1d94a8fc72e9c1f463c6c9f58ed9c87ff1af2519
Nathan VAN HEMELRYCK <vanhem_n@g02-r2p10.local>
2015-12-04 15:39:33 +0100

    pannier

--- Admin_xc96/index.php
+++ Admin_xc96/index.php
@@ -2,26 +2,24 @@
 
 <html lang='fr'>
 
-<link rel='stylesheet' href='style.css'>
+<link rel="stylesheet" href="style.css">
 
 <head>
-<meta http-equiv='Content-Type' content='text/html; charset=UTF-8' />
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 
 <title>E-quitable</title>
 </head>
 
 <body>
-<?php 
-session_start();
-if (isset($_SESSION['User_role']) AND $_SESSION['User_role'] == 1){
-echo "<!-- Header --> 
+
+<!-- Header --> 
 <div id='header'>
 
-<a href='#' class='bouton blanc large'>Gestion des Users</a>
-<a href='#' class='bouton blanc large'>Suppression elements</a>
-<a href='#' class='bouton blanc large'>Ajout elements</a>
-<a href='#' class='bouton blanc large'>Modification elements</a>
-<a href='#' class='bouton blanc large' style='margin-left : 35%'>Retour au site</a>
+<a href="#" class="bouton blanc large">Gestion des Users</a>
+<a href="#" class="bouton blanc large">Suppression elements</a>
+<a href="#" class="bouton blanc large">Ajout elements</a>
+<a href="#" class="bouton blanc large">Modification elements</a>
+<a href="#" class="bouton blanc large" style='margin-left : 35%'>Retour au site</a>
 </div>
 
 <center><div id='wrap'>
@@ -31,13 +29,7 @@ echo "<!-- Header -->
  <section id='moite'></section>
 
 </div></center>
-<embed autostart='true' loop='true' hidden='true' src='backsound.mp3'></embed>";
-}
-else{
-echo "Et non";
-}
-
-?>
+<embed autostart="true" loop="true" hidden="true" src="backsound.mp3"></embed>
 </body>
 
 </html>
