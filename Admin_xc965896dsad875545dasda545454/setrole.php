ed3afe482ccfca6a8939630b35d96c79b2f5f4ba
Nathan VAN HEMELRYCK <vanhem_n@g02-r2p10.local>
2015-12-04 16:14:18 +0100

    Admin

--- Admin_xc965896dsad875545dasda545454/setrole.php
+++ Admin_xc965896dsad875545dasda545454/setrole.php
@@ -2,7 +2,17 @@
 
 include('../pages/config.php');
 
-$conn = new PDO("mysql:host=$dbhost;dbname=$dbname;charset=utf8",$dbuser,$dbpass);
+try {
+    $conn = new PDO("mysql:host=$dbhost;dbname=$dbname;charset=utf8",$dbuser,$dbpass);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully"; 
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed (check config.php):   " . $e->getMessage();
+    }
+
 $reponse = $conn->query("SELECT * FROM User");
 while($donnees = $reponse->fetch()){
 
