180f1cfa585bb9f00b4890f63835f22e066390a1
Nathan VAN HEMELRYCK <vanhem_n@g02-r2p10.local>
2015-12-04 09:32:42 +0100

    categ dynamique #1

--- pages/categ.php
+++ pages/categ.php
@@ -5,10 +5,7 @@ $conn = new PDO("mysql:host=$dbhost;dbname=$dbname;charset=utf8",$dbuser,$dbpass
 $reponse = $conn->query("SELECT * FROM Categories WHERE ID>2");
 while ($donnees = $reponse->fetch()){
 
-}
-$reponse->closeCursor();
-
-  echo "<a href='listprod01.html?id=" . $donnees['ID'] . "'><section id='section03'>
+ echo "<a href='listprod01.html?id=" . $donnees['ID'] . "'><section id='section03'>
        <aside>
          <h1 id='tpop'>" . $donnees['Libelle'] . "</h1>
          <center><image src='" . $donnees['image'] . "' width='170'></image></center>
@@ -17,4 +14,7 @@ $reponse->closeCursor();
          <center><h4>" . $donnees['Description'] . "</h4></center>
        </article>
         </section></a>"
+
+}
+$reponse->closeCursor();
 ?>
