9227d91849f33accca2e9100a3bbba8d25c3a210
Nathan VAN HEMELRYCK <vanhem_n@g02-r2p10.local>
2015-12-04 11:06:04 +0100

    pannier

--- pages/cady.php
+++ pages/cady.php
@@ -1,8 +1,25 @@
 <?php
 include('config.php');
 session_start();
-$id = $_GET['id'];
-echo $id;
-echo "<br>";
-var_dump($_SESSION);
+$id_prod = $_GET['id'];
+
+ try {
+    $conn = new PDO("mysql:host=$dbhost;dbname=$dbname;charset=utf8",$dbuser,$dbpass);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully"; 
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed (check config.php):    " . $e->getMessage();
+    exit();
+    }
+
+    $id_user = $_SESSION['User_ID'];
+
+$result = $conn->prepare("INSERT INTO Produit_User (ID_produit,ID_utilisateur) VALUES (:ID_produit, :ID_utilisateur)");
+	$result->bindParam(':ID_produit', $id_prod);
+	$result->bindParam(':ID_utilisateur', $id_user);
+	$result->execute();
+	header("location pannier.php")
 ?>
